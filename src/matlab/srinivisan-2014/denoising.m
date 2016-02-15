510588892ad933e8276dcea6403a6e4682fd8f7f
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-16 00:31:12 +0100

    Add the dependency to denoising.m

--- src/matlab/srinivisan-2014/denoising.m
+++ src/matlab/srinivisan-2014/denoising.m
@@ -1,6 +1,10 @@
 function [ Oimg ] = denoising( Iimg )
 %DENOISING Summary of this function goes here
 %   Detailed explanation goes here
+
+    % First we need to add the path to BM3D
+    addpath ../../../third-party/BM3D
+
     Iimg=uint8(Iimg);
     sigma = std2(Iimg(800:900,100:200));
     [t,Oimg] = BM3D(1, Iimg,sigma);
