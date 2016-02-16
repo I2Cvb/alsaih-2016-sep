6b768e365e751652c9327b007c1a015b4d31054b
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-16 01:09:12 +0100

    Revision of the denoising
    Check the input image such that the input is scale as a float between 0. and 1.

--- src/matlab/srinivisan-2014/denoising.m
+++ src/matlab/srinivisan-2014/denoising.m
@@ -2,11 +2,25 @@ function [ Oimg ] = denoising( Iimg )
 %DENOISING Summary of this function goes here
 %   Detailed explanation goes here
 
-    % First we need to add the path to BM3D
-    addpath ../../../third-party/BM3D
+    % Add BM3D dependency
+    addpath('../../../third-party/BM3D');
 
-    Iimg=uint8(Iimg);
+    % Check the input type
+    if isfloat(Iimg)
+        % Check that the value are between 0. and 1.
+        if ( ( max(Iimg(:)) > 1. ) || ( min(Iimg(:)) < 0. ) )
+            error(['Image of type with value out of range. Need to ' ...
+                   'scale between 0. and 1.'])
+        end
+    elseif isinteger(Iimg)
+        % Convert the data to floating number
+        Iimg = im2double(Iimg);
+    end
+
+    % Estimate the noise variance from the image
     sigma = std2(Iimg(800:900,100:200));
-    [t,Oimg] = BM3D(1, Iimg,sigma);
+
+    % Apply the BM3D filter
+    [t, Oimg] = BM3D(1, Iimg, sigma);
 end
 
