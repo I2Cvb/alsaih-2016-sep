81285dd30c73ed5c1e2fa992eaf4fea78e942a04
Khaled Al-Saih <khaledalsaih@gmail.com>
2016-02-16 09:41:58 +0100

    Correct the syntax for addpath in denoising

--- src/matlab/srinivisan-2014/denoising.m
+++ src/matlab/srinivisan-2014/denoising.m
@@ -3,8 +3,8 @@ function [ Oimg ] = denoising( Iimg )
 %   Detailed explanation goes here
 
     % First we need to add the path to BM3D
-    addpath ../../../third-party/BM3D
-
+    addpath(../../../third-party/BM3D);
+    
     Iimg=uint8(Iimg);
     sigma = std2(Iimg(800:900,100:200));
     [t,Oimg] = BM3D(1, Iimg,sigma);
