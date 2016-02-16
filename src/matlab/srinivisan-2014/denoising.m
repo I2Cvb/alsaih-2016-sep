1d4031b028ff98a397eaf5dcfd11c7c7835c26da
Khaled Al-Saih <kalsaih@anakim@u-bourgogne.fr>
2016-02-16 10:26:22 +0100

    changing the hyphen for the path of BM3D

--- src/matlab/srinivisan-2014/denoising.m
+++ src/matlab/srinivisan-2014/denoising.m
@@ -3,9 +3,8 @@ function [ Oimg ] = denoising( Iimg, sigma )
 %   Detailed explanation goes here
 
     % First we need to add the path to BM3D
-    addpath(../../../third-party/BM3D);
+    addpath('../../../third-party/BM3D');
     
     Iimg=uint8(Iimg);
-    [t,Oimg] = BM3D(1, Iimg,sigma);
+    [t, Oimg] = BM3D(1, Iimg, sigma);
 end
-
