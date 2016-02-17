84e932f448124aac0d581015bc4a4abbbcac0b2d
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-17 20:42:11 +0100

    Update the flattening

--- src/matlab/srinivisan-2014/preprocessing/flattening.m
+++ src/matlab/srinivisan-2014/preprocessing/flattening.m
@@ -1,30 +1,40 @@
 function [ Oimg,ma ] = flattening( Iimg )
-%FLATTENING Summary of this function goes here
-%   Detailed explanation goes here
+    % Binarize the image to find the maximum
     m=im2bw(Iimg,0.3);
+    % Compute the convex hull
     d= bwconvhull(m,'objects');
+    %apply median filter
     B = medfilt2(d);
-    for i = 1:512
-    tt=find(B(:,i));
-    data(i)=tt(length(tt));
+    %find lower bounday
+    for i = 1:size(B,2)
+        tt=find(B(:,i));
+        if length(tt)==0
+            data(i)=NaN;
+        else
+            data(i)=tt(length(tt));
+        end
     end
-    p = polyfit(1:512,mean(data)*ones(1,512),2);
+
+    data(find(isnan(data)))=nanmean(data);
+    %second order polynomail for the lower boundary
+    p = polyfit(1:size(B,2),mean(data)*ones(1,size(B,2)),2);
+    %fit data to the calculated polynomial
     y2 = polyval(p,data);
     ap=data-y2;
-    for i = 1:512
-    if ap(i)>0
-    dif=abs(floor(ap(i)));
-    te=Iimg(:,i);
-    te=te([ dif+1:end 1:dif ]);
-    Iimg(:,i)=te;
-    else
-    dif=abs(floor(ap(i)));
-    te=Iimg(:,i);
-    te=te([ end-dif+1:end 1:end-dif ]);
-    Iimg(:,i)=te;
-    end
+    %movecolumns up or down
+    for i = 1:size(B,2)
+        if ap(i)>0
+            dif=abs(floor(ap(i)));
+            te=Iimg(:,i);
+            te=te([ dif+1:end 1:dif ]);
+            Iimg(:,i)=te;
+        else
+        dif=abs(floor(ap(i)));
+        te=Iimg(:,i);
+        te=te([ end-dif+1:end 1:end-dif ]);
+        Iimg(:,i)=te;
+        end
     end
     Oimg=Iimg;
     ma=mean(data);
 end
-
