e6cb26de740a9b67e521fd1f2697ad4f166f0866
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-17 20:21:42 +0100

    Finish to parallelize the denoising with an example

--- src/matlab/srinivisan-2014/pipeline/srinivisan_2014.m
+++ src/matlab/srinivisan-2014/pipeline/srinivisan_2014.m
@@ -12,7 +12,7 @@ y_size = 128;
 z_size = 1024;
 
 % Read the volume
-[ vol ] = read_oct_volume( '../../../../data/PCS57635OS.img', x_size, ...
+[ vol ] = read_oct_volume( '../../../../data/P741009OS.img', x_size, ...
           y_size, z_size );
 
 % Convert to double
@@ -21,11 +21,8 @@ vol = vol / max( vol(:) );
 % Define a fix sigma value 
 sigma = 190. / 255.;
 
-% We will make a parallel processing
-vol_out = zeros( size(vol) );
+poolobj = parpool('local', 40);
 
-parfor sl = 1 : size(vol, 3)
-    if sl <= size(vol, 3)
-        denoising( vol(:, :, sl), sigma );
-    end
-end
+vol_out = denoising_volume(vol, sigma);
+
+delete(poolobj);
