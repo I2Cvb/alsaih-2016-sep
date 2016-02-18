a6fb71b83b6c9452c15dc58c5f49b0b6b9d341e6
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-18 10:35:07 +0100

    Finish the pipeline properly parallelize

--- src/matlab/srinivisan-2014/pipeline/pipeline_preprocessing.m
+++ src/matlab/srinivisan-2014/pipeline/pipeline_preprocessing.m
@@ -11,13 +11,6 @@ x_size = 512;
 y_size = 128;
 z_size = 1024;
 
-% Read the volume
-[ vol ] = read_oct_volume( '../../../../data/P741009OS.img', x_size, ...
-          y_size, z_size );
-
-% Convert to double
-vol = vol / max( vol(:) );
-
 % Define a fix sigma value 
 sigma = 190. / 255.;
 
@@ -26,13 +19,42 @@ h_over_rpe = 90;
 h_under_rpe = 30;
 width_crop = 340;
 
+% Define the data directory
+data_directory = '/data/retinopathy/OCT/SERI/original_data/';
+store_directory = '/data/retinopathy/OCT/SERI/pre_processed_data/srinivasan_2014/'
+directory_info = dir(data_directory);
+
 poolobj = parpool('local', 40);
 
-vol_denoised = denoising_volume( vol, sigma );
+for idx_file = 1:size(data_directory)
 
-[ baseline_vol, vol_flattened ] = flattening_volume( vol_denoised );
+    % Get only of the extension is .img
+    if ( ~isempty( strfind( directory_info(idx_file).name, '.img' ) ...
+                   ) )
+        filename = strcat( data_directory, directory_info(idx_file).name ...
+                           );
 
-vol_cropped = crop_volume( vol_flattened, baseline_vol, h_over_rpe, ...
-                           h_under_rpe, width_crop );
+        % Read the volume
+        [ vol ] = read_oct_volume( filename, x_size, y_size, z_size ...
+                                   );
+
+        % Convert to double
+        vol = vol / max( vol(:) );
+
+        % Apply the preprocessing
+        vol_denoised = denoising_volume( vol, sigma );
+        [ baseline_vol, vol_flattened ] = flattening_volume( vol_denoised );
+        vol_cropped = crop_volume( vol_flattened, baseline_vol, h_over_rpe, ...
+                                   h_under_rpe, width_crop );
+
+        % Store the volume inside a mat file
+        store_filename = strcat( store_directory, strrep( ...
+            directory_info(idx_file).name, '.img', '.mat' ) );
+        save( store_filename, 'vol_cropped' );
+
+    end
+
+end
+
 
 delete(poolobj);
