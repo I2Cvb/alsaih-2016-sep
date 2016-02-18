86ccc7ebc00518590a0cc9d54f2410553ceafacc
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-02-18 21:23:25 +0100

    Improve the extraction of hog descriptor

--- src/matlab/srinivisan-2014/detection/extract_hog_volume.m
+++ src/matlab/srinivisan-2014/detection/extract_hog_volume.m
@@ -9,13 +9,8 @@ function [ feature_mat_vol ] = extract_hog_volume( in_vol, pyr_num_lev, CellSize
     for lev = 0:pyr_num_lev - 1
         % Compute the factor of reduction to apply on the size of
         % the image
-        factor = 2 * lev;
-        if factor ~= 0
-            im_sz = ceil( [ size(in_vol, 1) size(in_vol, 2) ] / ...
-                          factor );
-        else
-            im_sz = [ size(in_vol, 1) size(in_vol, 2) ];
-        end
+        factor = 2 ^ lev;
+        im_sz = ceil( [ size(in_vol, 1) size(in_vol, 2) ] / factor );
 
         blocksimage = floor( ( im_sz ./ CellSize - BlockSize ) ./ ( BlockSize - BlockOverlap ) + 1 );
         feat_dim = feat_dim + prod([blocksimage, BlockSize, NumBins]);
@@ -24,7 +19,7 @@ function [ feature_mat_vol ] = extract_hog_volume( in_vol, pyr_num_lev, CellSize
     % Pre-allocate feature_mat_vol
     feature_mat_vol = zeros( size(in_vol, 3), feat_dim );
 
-    parfor sl = 1 : size(in_vol, 3)
+    for sl = 1 : size(in_vol, 3)
         if ( sl <= size(in_vol, 3) )
             feature_mat_vol(sl, :) = extract_hog_image( in_vol(:, :, sl), ...
                                                         pyr_num_lev, ...
@@ -45,37 +40,35 @@ function [ feature_vec_img ] = extract_hog_image( in_img, pyr_num_lev, CellSize,
     for lev = 0:pyr_num_lev - 1
         % Compute the factor of reduction to apply on the size of
         % the image
-        factor = 2 * lev;
-        if factor ~= 0
-            im_sz = ceil( size(in_img) / factor );
-        else
-            im_sz = size(in_img);
-        end
-        
+        factor = 2 ^ lev;
+        im_sz = ceil( size(in_img) / factor );
+       
         blocksimage = floor( ( im_sz ./ CellSize - BlockSize ) ./ ( BlockSize - BlockOverlap ) + 1 );
         feat_dim = [ feat_dim, prod([blocksimage, BlockSize, NumBins]) ...
                      ];
     end
 
     % Make the allocation
-    feature_vec_img = zeros( sum(feat_dim) );
-    cum_feat_dim = [ 1 cumsum( feat_dim ) ];
+    feature_vec_img = zeros( 1, sum(feat_dim) );
+    cum_feat_dim = [ 0 cumsum( feat_dim ) ];
 
     for lev = 1:pyr_num_lev
         % Downsize if necessary
         im_rsz = in_img;
         if ( lev > 1 )
-            for rsz = 1:lev
+            for rsz = 1:lev-1
                 im_rsz = impyramid(im_rsz, 'reduce');
             end
         end
         % Compute the HOG feature
-        feature_vec_img( cum_feat_dim(lev) : cum_feat_dim(lev + 1) ) = ...
-            extractHOGFeatures(im_rsz,'CellSize', CellSize, ...
+        feature_vec_img( cum_feat_dim(lev) + 1 : cum_feat_dim(lev + 1) ) = ...
+            extractHOGFeatures(im_rsz, 'CellSize', CellSize, ...
                                'BlockSize', BlockSize,'BlockOverlap', ...
                                BlockOverlap);
 
     end
+
+    disp(size(feature_vec_img));
 
 end
 
