ced6968f3fe3bb2edbd3c54ec4fd6bcf25222455
Khaled Al-Saih <kalsaih@anakim@u-bourgogne.fr>
2016-02-19 23:37:04 +0100

    fitting the dimensions of label set, train set , test set

--- src/matlab/srinivisan-2014/pipeline/pipeline_classifier.m
+++ src/matlab/srinivisan-2014/pipeline/pipeline_classifier.m
@@ -1,28 +1,33 @@
+
 clc
 clear all
 
 file = dir('/data/retinopathy/OCT/SERI/feature_data/srinivasan_2014/*.mat');
-[n,s,c]=xlsread('data.csv');
+[n,s,c]=xlsread('../../../../data/data.csv');
 s=char(s{2:size(s,1),1});
-train_set = []; 
-test_set = []; 
-train_label = []; 
-test_label = [];
 index=[0 16];
 gpf=[];
 ladf=[];
 %%% Loading the test set 
 for i = 1 : 16
         index = index + 1;
+        train_set = [];
+        test_set = [];
+        train_label = [];
+        test_label = [];
     for j = 1:32
-    temp=find(strncmp( cellstr(s), strtok(file(i).name,'.'), length(strtok(file(i).name,'.'))));
-    temp2=strcat('/data/retinopathy/OCT/SERI/feature_data/srinivasan_2014/',file(i).name);
+    temp=find(strncmp( cellstr(s), strtok(file(j).name,'.'), length(strtok(file(j).name,'.'))));
+    temp2=strcat('/data/retinopathy/OCT/SERI/feature_data/srinivasan_2014/',file(j).name);
+    %to check the loop is going well becuase the mistake before
+    % i used i in the file names while it should be in j
+    %file(i).name
+    %n(temp)
     load(temp2)
     if length(find(j==index))>0
           if n(temp)==1
               test_set = [test_set; hog_feat]; 
               label_tem = ones(128,1); 
-              test_label = [train_label; label_tem ];
+              test_label = [test_label; label_tem ];
           else
               test_set = [test_set; hog_feat]; 
               label_tem = -1*ones(128,1); 
@@ -41,6 +46,9 @@ for i = 1 : 16
           end
     end   
     end
+    %checker for the size of the label after each iteration   
+    size(test_label)
+    % size(test_set)
     %%% Training the svm classifier using training set
     SVMStruct = svmtrain(train_set,train_label);
     
