253dd2db4376391fb0594bc8584d4949021e7e4f
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-03-11 00:18:49 +0100

    Start to check the energies of the eigenvalues during PCA decomposition

--- src/matlab/srinivisan-2014/pipeline/pipeline_classifier_PCA.m
+++ src/matlab/srinivisan-2014/pipeline/pipeline_classifier_PCA.m
@@ -1,85 +1,105 @@
-clc
-clear all
+clear all;
+close all;
+clc;
 
-file = dir('/data/retinopathy/OCT/SERI/feature_data/srinivasan_2014/*.mat');
-[n,s,c]=xlsread('../../../../data/data.csv');
-s=char(s{2:size(s,1),1});
-index=[0 16];
-gpf=[];
-ladf=[];
-%%% Loading the test set 
-for i = 1 : 16
-	  index = index + 1;
-train_set = []; 
-train_label = []; 
-for j = 1:32
-	  temp=find(strncmp( cellstr(s), strtok(file(j).name,'.'), length(strtok(file(j).name,'.'))));
-temp2=strcat('/data/retinopathy/OCT/SERI/feature_data/srinivasan_2014/',file(j).name);
-load(temp2)
-if length(find(j==index))==0
-  %           if n(temp)==1
-  %               test_set = [test_set; hog_feat]; 
-%               label_tem = ones(128,1); 
-%               test_label = [test_label; label_tem ];
-%           else
-	      %               test_set = [test_set; hog_feat]; 
-%               label_tem = -1*ones(128,1); 
-%               test_label = [test_label; label_tem ];
-%           end
-% 
-%     else
-	if n(temp)==1
-  train_set = [train_set; hog_feat]; 
-label_tem = ones(128,1); 
-train_label = [train_label; label_tem ];
- else
-   train_set = [train_set; hog_feat]; 
-label_tem = -1*ones(128,1); 
-train_label = [train_label; label_tem ];
-          end
-    end   
+% Give the information about the data location
+% Location of the features
+data_directory = ['/data/retinopathy/OCT/SERI/feature_data/' ...
+                  'srinivasan_2014/'];
+% Location to store the results
+store_directory = ['/data/retinopathy/OCT/SERI/results/' ...
+                   'srinivasan_2014/'];
+% Location of the ground-truth
+gt_file = '../../../../data/data.csv';
+
+% Load the csv data
+[~, ~, raw_data] = xlsread(gt_file);
+% Extract the information from the raw data
+% Store the filename inside a cell
+filename = { raw_data{ 2:end, 1} };
+% Store the label information into a vector
+data_label = [ raw_data{ 2:end, 2 } ];
+% Get the index of positive and negative class
+idx_class_pos = find( data_label ==  1 );
+idx_class_neg = find( data_label == -1 );
+
+% poolobj = parpool('local', 48);
+
+% Pre-allocate where the data will be locate
+pred_label_cv = zeros( length(idx_class_pos), 2 );
+
+% Cross-validation using Leave-Two-Patients-Out
+for idx_cv_lpo = 1:length(idx_class_pos)
+    disp([ 'Round #', num2str(idx_cv_lpo), ' of the L2PO']);
+
+    % The two patients for testing will corresspond to the current
+    % index of the cross-validation
+
+    % CREATE THE TESTING SET
+    testing_data = [];
+    testing_label = [];
+    % Load the positive patient
+    load( strcat( data_directory, filename{ idx_class_pos(idx_cv_lpo) ...
+                   } ) );
+    % Concatenate the data
+    testing_data = [ testing_data ; hog_feat ];
+    % Create and concatenate the label
+    testing_label = [ testing_label ones(1, size(hog_feat, 1)) ];
+    % Load the negative patient
+    load( strcat( data_directory, filename{ idx_class_neg(idx_cv_lpo) ...
+                   } ) );
+    % Concatenate the data
+    testing_data = [ testing_data ; hog_feat ];
+    % Create and concatenate the label
+    testing_label = [ testing_label ( -1 * ones(1, size(hog_feat, 1))) ];
+
+    disp('Created the testing set');
+
+    % CREATE THE TRAINING SET
+    training_data = [];
+    training_label = [];
+    for tr_idx = 1:length(idx_class_pos)
+        % Consider only the data which where not used for the
+        % testing set
+        if ( tr_idx ~= idx_cv_lpo)
+            % Load the positive patient
+            load( strcat( data_directory, filename{ idx_class_pos(tr_idx) ...
+                   } ) );
+            % Concatenate the data
+            training_data = [ training_data ; hog_feat ];
+            % Create and concatenate the label
+            training_label = [ training_label ones(1, size(hog_feat, 1)) ];
+            % Load the negative patient
+            load( strcat( data_directory, filename{ idx_class_neg(tr_idx) ...
+                   } ) );
+            % Concatenate the data
+            training_data = [ training_data ; hog_feat ];
+            % Create and concatenate the label
+            training_label = [ training_label (-1 * ones(1, size(hog_feat, 1))) ];
+        end
     end
 
-	  
-	  mn = mean(train_set);
-train_set = bsxfun(@minus,train_set,mn); % substract mean
-[coefs,scores,variances] = princomp(train_set,'econ'); % PCA
-dims = 9;
-train_set = train_set*coefs(:,1:dims); % dims - keep this many dimensions
+    disp('Created the training set');
 
-    %%% Training the svm classifier using training set
-SVMStruct = svmtrain(train_set,train_label);
-    
-%%% Test the svm classifier using the test set
-for j = 1 : 2
-	  test_set = [];
-test_label = [];
-temp=find(strncmp( cellstr(s), strtok(file(index(j)).name,'.'), length(strtok(file(index(j)).name,'.'))));
-temp2=strcat('/data/retinopathy/OCT/SERI/feature_data/srinivasan_2014/',file(index(j)).name);
-load(temp2)
-if n(temp)==1
-  test_set = [test_set; hog_feat]; 
-label_tem = ones(128,1); 
-test_label = [test_label; label_tem ];
- else
-   test_set = [test_set; hog_feat]; 
-label_tem = -1*ones(128,1); 
-test_label = [test_label; label_tem ];
-          end
-	  test_set = bsxfun(@minus,test_set,mn);
-test_set = test_set*coefs(:,1:dims); % result is in train_out and test_out
-Group = svmclassify(SVMStruct, test_set);
-gpf=[gpf;mode(Group)];
-ladf=[ladf;mode(test_label)];
-    end
+    [coeff,score,latent,tsquared,explained,mu] = pca(training_data);
+    disp(explained);
+
+    % % Perform the training of the SVM
+    % svmStruct = svmtrain( training_data, training_label );
+    % disp('Trained SVM classifier');
+    % % Test the performance of the SVM
+    % pred_label = svmclassify(svmStruct, testing_data);
+    % disp('Tested SVM classifier');
+
+    % % We need to split the data to get a prediction for each volume
+    % % tested
+    % % Compute the majority voting for each testing volume
+    % maj_vot = [ mode( pred_label(1:size(hog_feat,1)) ) ...
+    %             mode( pred_label(size(hog_feat, 1) + 1:end) )];
+    % pred_label_cv( idx_cv_lpo, : ) = maj_vot;    
+    % disp('Applied majority voting');
 end
 
-    
-%%% Validate the results with test label
-%%%% confmat 
+save(strcat(store_directory, 'predicition_PCA.mat'), 'pred_label_cv');
 
-    C = confusionmat(gpf,ladf)
-      sensitivity = C(1,1)/C(1,1)+C(1,2)
-      specificity = C(2,2)/C(2,1)+C(2,2)
-      accuracy = C(1,1)+C(2,2)/C(1,1)+C(1,2)+C(2,1)+C(2,2)
-
+%delete(poolobj);
